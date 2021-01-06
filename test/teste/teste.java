0b86d5908dadd5575d95b2904c55fc56828519bf
Admnistrador <Admnistrador@DESKTOP-B4ERJ5H>
2021-01-06 11:58:10 -0300

    initialize repository

---
+++ test/teste/teste.java
@@ -0,0 +1,57 @@
+package teste;
+
+import RobotEfiorezeImportation.RobotEfiorezeImportation;
+import com.aspose.pdf.Document;
+import com.aspose.pdf.ExcelSaveOptions;
+import java.util.Map;
+import java.util.HashMap;
+
+public class teste {
+
+    public static void main(String[] args) {
+        test();
+    }
+    public static void convertPDF(){
+        Document doc =  new Document("G:\\Contábil\\Clientes\\E.Fioreze\\Escrituração mensal\\2020\\Extratos\\11.2020\\banriteste.pdf");
+        // Set Excel options
+        ExcelSaveOptions options = new ExcelSaveOptions();
+        // Set output format
+        options.setFormat(ExcelSaveOptions.ExcelFormat.XLSX);
+        // Set minimizing option
+        options.setMinimizeTheNumberOfWorksheets(true);
+        // Convert PDF to XLSX
+        doc.save("workbook.xlsx", options);
+    }
+    
+    public static void test() {
+        int mes = 11;
+        int ano = 2020;
+        String pastaEmpresa = "E.Fioreze";
+        String pastaAnual = "Extratos";
+        String pastaMensal = "";
+        String banco = "Template Banrisul teste (5)";
+        String idTemplate = "efioreze13";
+        String filtroArquivo = "banrisul;conci;.xlsx";
+        Map<String, String> colunas = new HashMap<>();
+        colunas.put("data", "B");
+        //colunas.put("documento", "");
+        colunas.put("pretexto", "");
+        colunas.put("historico", "G");
+        colunas.put("entrada", "D");
+        colunas.put("saida", "-E");
+        //colunas.put("valor", "");
+
+        System.out.println(RobotEfiorezeImportation.principal(
+                mes,
+                ano,
+                pastaEmpresa,
+                pastaAnual,
+                pastaMensal,
+                banco,
+                idTemplate,
+                filtroArquivo,
+                colunas
+        ).replaceAll("<br>", "\n")
+        );
+    }
+}
