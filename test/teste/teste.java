1f41c3e35121ae633dfbf3abd8f2e5f94c88b49d
Admnistrador <Admnistrador@DESKTOP-B4ERJ5H>
2021-01-18 16:20:26 -0300

    update test

--- test/teste/teste.java
+++ test/teste/teste.java
@@ -3,30 +3,41 @@ package teste;
 import RobotEfiorezeImportation.RobotEfiorezeImportation;
 import com.aspose.pdf.Document;
 import com.aspose.pdf.ExcelSaveOptions;
-import java.io.File;
-import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.List;
 
 public class teste {
 
     public static void main(String[] args) {
-        test();
+        List<String> str = new ArrayList<>();
+        str.add("first");
+        str.add("1");
+        str.add("2");
+        str.add("3");
+        str.add("4");
+        str.add("5");
+
+        while (str.size() > 1) {
+            str.remove(str.size() - 1);
+        }
+
+        System.out.println(str);
     }
-    public static void regexText(){
+
+    public static void regexText() {
         String str1 = "Pgto.doc : 012357/4 - Passalacqua (EST02160/4)";
         String str2 = "          -2.098,96";
-        
+
         String regex = "[^,]+";
-        
+
         System.out.println("Regex 1: " + str1.matches(regex));
         System.out.println("Regex 2: " + str2.matches(regex));
     }
-    
-    public static void convertPDF(){
-        Document doc =  new Document("G:\\Contábil\\Clientes\\E.Fioreze\\Escrituração mensal\\2020\\Extratos\\11.2020\\banriteste.pdf");
+
+    public static void convertPDF() {
+        Document doc = new Document("G:\\Contábil\\Clientes\\E.Fioreze\\Escrituração mensal\\2020\\Extratos\\11.2020\\banriteste.pdf");
         // Set Excel options
         ExcelSaveOptions options = new ExcelSaveOptions();
         // Set output format
@@ -36,7 +47,7 @@ public class teste {
         // Convert PDF to XLSX
         doc.save("workbook.xlsx", options);
     }
-    
+
     public static void test() {
         int mes = 12;
         int ano = 2020;
